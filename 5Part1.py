6a23da2482218b84269b7e8f402d7144ae4fac29
Shadow <pratiksanap245@gmail.com>
2025-12-23 20:46:31 +0530

    Update 5Part1.py

--- 5Part1.py
+++ 5Part1.py
@@ -2,11 +2,6 @@ with open("inp5.txt") as f:
     inp1 = f.read().splitlines()
 with open("inp52.txt") as g:
     inp2 = g.read().splitlines()
-for i in range(len(inp1)):
-        s=inp1[i]
-        index=s.index('-')
-        s1=int(s[0:index])
-        s2=int(s[index+1:])
 cnt=0
 for j in range(len(inp2)):
     for i in range(len(inp1)):
@@ -17,4 +12,5 @@ for j in range(len(inp2)):
         if int(inp2[j]) in range(s1,s2+1):
             cnt+=1
             break
+
 print(cnt)
